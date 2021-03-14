6bfac3dfa774c7bdcad9176d2bbf6e4af67b30ce
MaxKorbut <max.korbut0@gmail.com>
2021-03-14 14:40:37 +0300

    Final test_case_1

--- tests/test_case_1.py
+++ tests/test_case_1.py
@@ -10,13 +10,15 @@ def test_case_1(browser, get_data_for_admin_login, get_data_for_add_user):
     admin_page.fill_login_form(admin_username, admin_passwd)
 
     admin_user_page = UsersAdminPage(browser)
-    admin_user_page.go_to_users_page()
+    admin_user_page.go_to_page_from_auth_app_block("Users")
     new_username, new_passwd = get_data_for_add_user
     admin_user_page.add_new_user(new_username, new_passwd)
+    admin_user_page.give_staff_status_to_user()
 
     db = DataBase()
     db.is_user_in_auth_table(new_username)
 
-    # admin_page.do_something_on_top_toolbar("Log out")
-    # admin_page.do_something_on_top_toolbar("Django administration")
-    # admin_page.fill_login_form(new_username, new_passwd)
+    admin_page.go_to_something_page_from_top_toolbar("Log out")
+    admin_page.go_to_something_page_from_top_toolbar("Django administration")
+    admin_page.fill_login_form(new_username, new_passwd)
+    admin_page.is_login_successful(new_username)
