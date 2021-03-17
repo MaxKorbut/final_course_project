e6aabc844b3599ead581111185f5e8933d4ac4fb
MaxKorbut <max.korbut0@gmail.com>
2021-03-17 13:28:34 +0300

    db

--- database/db.py
+++ database/db.py
@@ -8,7 +8,7 @@ class DataBase:
             dbname='postgres',
             user='postgres',
             password='postgres',
-            host='172.18.0.4')
+            host='localhost')
         self.cursor = self.conn.cursor()
 
     def get_all_table(self):
